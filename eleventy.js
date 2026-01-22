29ba6eb0176f5b2c871088bedc0f3dbafaaf4132
ebeshero <ebbondar@gmail.com>
2026-01-22 16:06:27 -0500

    eleventy.js with source config

---
+++ eleventy.js
@@ -0,0 +1,22 @@
+module.exports = function (eleventyConfig) {
+   // Copy `_src/img/` to `_site/img`
+   eleventyConfig.addPassthroughCopy("_src/img");
+   eleventyConfig.addPassthroughCopy("_src/bundle.css");
+   eleventyConfig.addPassthroughCopy({ "_src/img/favicon.ico": "/" });
+   eleventyConfig.addPassthroughCopy("_src/includes/")
+
+
+ return {
+   dir: {
+     input: "source",
+     output: '_site'
+
+
+  },
+
+
+};
+  
+
+
+};
