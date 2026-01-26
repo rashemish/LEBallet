d5a2609dadd59f62b91345b7d82a403034f127a5
ebeshero <ebbondar@gmail.com>
2026-01-26 15:54:24 -0500

    repair for CSS to work on GitHub Pages website

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,12 @@
 module.exports = function (eleventyConfig) {
    // Copy `_src/img/` to `_site/img`
    eleventyConfig.addPassthroughCopy("src/img");
-   eleventyConfig.addPassthroughCopy("src/style.css");
+   eleventyConfig.addPassthroughCopy("src/css/style.css");
    eleventyConfig.addPassthroughCopy({ "src/img/favicon.ico": "/" });
 
 
  return {
-   dir: {
+     dir: {
      input: "src",
      output: '_site'
 
