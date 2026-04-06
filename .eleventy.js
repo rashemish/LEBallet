522fd47902e2181a9c4957bbf0948461d547f6c0
rashemish <rvm5988@psu.edu>
2026-04-06 12:39:17 -0400

    cleaning repo

--- .eleventy.js
+++ .eleventy.js
@@ -3,15 +3,11 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/assets");
 
-    eleventyConfig.addFilter("base", function(url) {
-        return "/LEBallet" + url;
-    });
-
     return {
         dir: {
             input: "src",
             includes: "_includes",
-            output: "docs"
+            output: "_site"
         }
     };
 };
